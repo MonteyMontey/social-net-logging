d3618bb52fa2c1f3b1e93bf6dfb018e58b3340b2
monteymontey <dominik.klein@code.berlin>
2019-12-06 18:00:05 +0100

    make log collection capped

--- app/models/Log.js
+++ app/models/Log.js
@@ -13,6 +13,9 @@ const LogSchema = new Schema({
     type: Date,
     default: Date.now
   }
-});
+}, {capped: {size: 5242880, max: 10000 }} );
+
+// max = maximum number of documents
+// size = maximum size of collection in bytes
 
 mongoose.model('logs', LogSchema);
